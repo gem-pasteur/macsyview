435e12a1e158bfcf452e5079d50062a0d1f6cc92
Hervé Ménager <hmenager@pasteur.fr>
2014-02-24 13:57:52 +0000

    improve application structure, work in progress...
    Former-commit-id: 7b4d75ec189aad30dd342fc62ea89ef00ec0eb00

--- TXSSView_yo/app/scripts/main.js
+++ TXSSView_yo/app/scripts/main.js
@@ -1,83 +1,103 @@
-function loadFile(evt) {
-    $('#contents').empty();
-    var files = evt.target.files; // FileList object
-    var chunkSize = 20000;
-    for (var i = 0, f; f = files[i]; i++) {
-        var fileSize = f.size;
-        $("#progress").attr('max', fileSize);
-        $("#progress").attr('value', 0);
+/*global $, FileReader, Mustache */
+
+(function () {
+    'use strict';
+    function loadFile(textFile, loadedCallback) {
+        var result = "";
+        var chunkSize = 20000;
+        var fileSize = textFile.size;
 
         function readBlob(file, offset) {
             var stop = offset + chunkSize - 1;
             if (stop > (fileSize - 1)) {
-                stop = fileSize - 1
-            };
+                stop = fileSize - 1;
+            }
             var reader = new FileReader();
             // If we use onloadend, we need to check the readyState.
             reader.onloadend = function (evt) {
-                if (evt.target.readyState == FileReader.DONE) { // DONE == 2
-                    $('#contents').append(evt.target.result);
-                    $("#progress").attr('value', stop);
+                if (evt.target.readyState === FileReader.DONE) { // DONE == 2
+                    result += evt.target.result;
                     if (stop < fileSize - 1) {
                         offset = offset + chunkSize;
                         evt = null;
-                        //$('.progress-label').text('loading ('+offset+'/'+fileSize+')...');
                         readBlob(file, offset);
                     } else {
-                        //$('.progress-label').text('file loaded!');
+                        loadedCallback(result);
                     }
                 }
             };
             var blob = file.slice(offset, stop + 1);
             reader.readAsBinaryString(blob);
         }
-        readBlob(f, 0);
+        readBlob(textFile, 0);
     }
-}
 
-var output = $("#contents");
+    var viewContainer = $("#mainView");
 
-var listSystemMatchFiles = function (files) {
-    var systemMatchRE = /([A-Za-z0-9]+)_([A-Za-z0-9]+)_([A-Za-z0-9]+)\.json/;
-    var systemMatchFiles = [];
-    for (var i = 0, len = files.length; i < len; i++) {
-        file = files[i];
-        var match = systemMatchRE.exec(file.name)
-        if (match) {
-            systemMatchFiles.push({
-                'file': file,
-                'replicon_name': match[1],
-                'system_name': match[2],
-                'occurence_number': match[3],
-                'id':file.name
-            });
+    var displayView = function (viewName, context) {
+        viewContainer.html('');
+        var template = $('#' + viewName).text();
+        viewContainer.html(Mustache.render(template, context));
+    };
+
+    var displaySelectForm = function () {
+        displayView('runSelectForm', {});
+        $('#systemMatchesLinkList').hide();
+        $('#directory').change(directorySelectionHandler);
+    };
+
+    var listSystemMatchFiles = function (files) {
+        var systemMatchRE = /([A-Za-z0-9]+)_([A-Za-z0-9]+)_([A-Za-z0-9]+)\.json/;
+        var systemMatchFiles = [];
+        for (var i = 0, len = files.length; i < len; i++) {
+            var match = systemMatchRE.exec(files[i].name)
+            if (match) {
+                systemMatchFiles.push({
+                    'file': files[i],
+                    'replicon_name': match[1],
+                    'system_name': match[2],
+                    'occurence_number': match[3],
+                    'id': files[i].name
+                });
+            }
         }
-    }
-    return systemMatchFiles;
-}
+        return systemMatchFiles;
+    };
 
-var displaySystemMatchFileDetail = function(){
-}
+    var displaySystemMatchFileDetail = function (systemMatch) {
+        loadFile(systemMatch.file, function (contentsText) {
+            var doc = JSON.parse(contentsText);
+            doc.matchedGenes = doc.genes.filter(function (gene) {
+                return ('match' in gene);
+            });
+            displayView('systemMatchDetail', doc);
+        });
+    };
 
-var initSystemMatchSelectionHandler = function (systemMatchFiles) {
-    $(".txsview-systemmatchtablerow td").click(function(e){
-        var id = $(e.currentTarget).parent().attr('data-systemmatchid');
-        var selectedSystemMatchFile = systemMatchFiles.filter(function(systemMatchFile){
-            return systemMatchFile.id == id;
-        })[0];
-        displaySystemMatchFileDetail(selectedSystemMatchFile);
+
+
+    var initSystemMatchSelectionHandler = function (systemMatchFiles) {
+        $(".txsview-systemmatchtablerow td").click(function (e) {
+            var id = $(e.currentTarget).parent().attr('data-systemmatchid');
+            var selectedSystemMatchFile = systemMatchFiles.filter(function (systemMatchFile) {
+                return systemMatchFile.id == id;
+            })[0];
+            displaySystemMatchFileDetail(selectedSystemMatchFile);
+        });
+    };
+
+    var directorySelectionHandler = function (e) {
+        var files = e.target.files;
+        var systemMatchFiles = listSystemMatchFiles(files);
+        displayView('systemMatchesList', {
+            'files': systemMatchFiles
+        });
+        initSystemMatchSelectionHandler(systemMatchFiles);
+    };
+
+    $(document).ready(function () {
+        displaySelectForm();
+        $('#homeLink').click(displaySelectForm);
+
     });
-}
-
-var directorySelectionHandler = function (e) {
-    output.innerHTML = "";
-    var files = e.target.files;
-    var systemMatchFiles = listSystemMatchFiles(files);
-    var template = '<table class="table table-hover tab-striped"><tr><th>Replicon Name</th><th>System Name</th><th>Occurence No.</th></tr>{{#files}}<tr class="txsview-systemmatchtablerow" data-systemmatchid="{{id}}"><td>{{replicon_name}}</td><td>{{system_name}}</td><td>{{occurence_number}}</td></tr>{{/files}}</table>';
-    output.html(Mustache.render(template, {
-        'files': systemMatchFiles
-    }));
-    initSystemMatchSelectionHandler(systemMatchFiles);
-}
-
-$('#directory').change(directorySelectionHandler);
+}());
