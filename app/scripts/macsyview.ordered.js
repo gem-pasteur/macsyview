aa4510c2da24d5b3932cabb959d050dcd82ea356
Bertrand Néron <bneron@pasteur.fr>
2014-03-21 16:12:40 +0000

    add js module to display ordered replicon (macsyview.ordered.js) add html to test macsyview.ordered.js add some gene color
    Former-commit-id: 8ca4ba4bcf16029dc7fcfe1e4bf9550493fe3ef2

---
+++ app/scripts/macsyview.ordered.js
@@ -0,0 +1,167 @@
+
+/*
+ * macsyview.orderedview
+ * Ordered view for MacSyView
+ * requires RaphaelJS + ???
+ */
+
+/* jslint         browser : true, continue : true,
+  devel  : true, indent  : 2,    maxerr   : 50,
+  newcap : true, nomen   : true, plusplus : true,
+  regexp : true, sloppy  : true, vars     : false,
+  white  : true
+ */
+
+/*global $, macsyview */
+
+macsyview.orderedview = (function () {
+	'use strict';
+
+	var configMap = {
+			paper_h : 250,
+			paper_w : 500,
+			y_replicon : 55,
+			replicon_offset : 40,
+			genes_offset : 20,
+			gene_high : 20,
+			inter_gene_space : 20,
+			gene_infos_container : $('<div id="gene_infos"></div>'),
+			gene_infos_tpl : $('#gene_info_Tpl'),
+	};
+
+	/***********************
+	 *  Business objects
+	 ***********************/
+
+	/*************************
+	 *        Replicon
+	 *************************/
+	var Replicon = function Replicon(json_data){
+		this.genes = [];
+		var genes_number = json_data.genes.length;
+		for (var i = 0; i < genes_number; i++){
+			this.genes[i] = new Gene(this, json_data.genes[i]);
+			if (i != 0){
+				this.genes[i].start = this.genes[i-1].start + this.genes[i-1].length + configMap.inter_gene_space;
+			}else{
+				this.genes[i].start = 0;
+			}
+			console.log(this.genes[i].start);
+		}
+		//in number of pixels
+		this.length = this.genes[this.genes.length - 1].start + (configMap.genes_offset * 2);
+	};
+
+	/*************************
+	 *        Gene
+	 *************************/
+
+	var Gene = function Gene(replicon, json_gene){
+		this.replicon = replicon;
+		this.name = json_gene.name;
+		this.position = json_gene.position;
+		this.length = json_gene.sequence_length;
+		this.match = json_gene.match;
+		this.start = null;
+	};
+
+	/***************************
+	 *      View objects
+	 ***************************/
+
+
+	/******************
+	 *  RepliconGrphx
+	 ******************/
+	var RepliconGrphx = function RepliconGrphx(replicon){
+		this.graph = null;
+		this.replicon = replicon;
+		this.genes = [];
+		console.log("this.replicon.genes_number = ", this.replicon.genes_number);
+		for (var i = 0; i < this.replicon.genes.length; i++){
+			this.genes[i] = new GenesGrphx(this, this.replicon.genes[i]);
+		};
+	};
+
+	RepliconGrphx.prototype.draw = function draw_replicon(paper){
+		var repl_len_in_px = configMap.paper_w - (2 * configMap.replicon_offset);
+		var p = $('<p class="replicon_body"></p>').hide().appendTo("body");
+		var replicon_color = p.css("background-color");
+		console.log("replicon_color = ", replicon_color);
+		p.remove();
+		console
+		this.graph = paper.set();
+		var genome = paper.path(["M, ", configMap.replicon_offset, configMap.y_replicon, 
+		                   "h", repl_len_in_px, "a25,5 -1 0,1 0,5h", 
+		                   (-1 * (repl_len_in_px)), "a25,5 0 0,1 0,-5z"]).attr({"fill": replicon_color, 
+		                	   "stroke": replicon_color, 
+		                	   "stroke-width":"1"
+		                   } );
+		this.graph.push(genome);
+		console.log( "this.genes.length", this.genes.length);
+		for (var i = 0; i < this.genes.length; i++){
+			console.log("appel de gene.draw");
+			var g = this.genes[i].draw(paper);
+			this.graph.push(g);
+		};
+	};
+
+	/*****************
+	 *   GenesGrphx
+	 *****************/
+
+	var GenesGrphx = function GenesGrphx(repGrphx, gene){
+		this.replicon = repGrphx;
+		this.graph = null;
+		this.gene = gene;
+	};
+
+	GenesGrphx.prototype.draw = function draw_gene(paper){
+		var x = this.gene.start;
+		var y = configMap.y_replicon - (configMap.gene_high / 2) ; 
+		var w = this.gene.length; 
+		var h = configMap.gene_high;
+		console.log( "x =",x," y = ",y, " w = ",w+" h = "+h);
+		var arrow = paper.rect(x, y, w, h);
+		console.log( "this.gene.match =", this.gene.match);
+		if(!this.gene.match){
+			arrow.attr({fill: "white", 
+				stroke: "black", 
+				"stroke-dasharray": "-", 
+				"fill-opacity": 0.5}
+			);
+		}else{
+			var color = $(".gene_" + this.match).css("background-color");
+			arrow.attr({fill: color, stroke: "none", "fill-opacity": 0.9});
+		};
+		return arrow;
+	};
+
+	GenesGrphx.prototype.show = function show_gene(){
+		var template = configMap.gene_infos_tpl.html();
+		var html = Mustache.to_html(template, this);
+		configMap.gene_infos_container.html(html);
+	};
+
+	GenesGrphx.prototype.hide = function hide_gene(){
+		configMap.gene_infos_container.html("fly cursor over a gene to display informations");
+	};
+
+
+	var draw = function(json_data, container){
+		var replicon = new Replicon(json_data);
+
+		var paper = Raphael(container, configMap.paper_w, configMap.paper_h );
+		console.log( "container = ", container , "w = ",configMap.paper_w, " h = ",configMap.paper_h);
+		paper.canvas.style.backgroundColor = '#F00';
+		var repliconGrphx = new RepliconGrphx(replicon);
+		repliconGrphx.draw(paper);
+
+	};
+	return {
+		configMap: configMap,
+		draw: draw,
+	};
+}());
+
+
