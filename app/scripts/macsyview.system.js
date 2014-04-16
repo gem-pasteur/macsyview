e545d422bea2e958d0edbcccd72626a929a6f65a
Hervé Ménager <hmenager@pasteur.fr>
2014-04-16 09:06:07 +0000

    do not color non-occuring components, and correct js style.
    Former-commit-id: a5b7642b67b9626946758ffce6b8f1f5761c829a

--- app/scripts/macsyview.system.js
+++ app/scripts/macsyview.system.js
@@ -16,34 +16,33 @@
 macsyview.system = (function () {
 	'use strict';
 
-	var colorMap = {
-			"mandatory" : "green",
-			"allowed" : "blue",
-			"forbidden" : "red",
-			"empty" : "grey"
-	};
+	var presence = ["mandatory", "allowed", "forbidden"],
 	
-	var presence = ["mandatory", "allowed", "forbidden"];
-	
-	var draw = function(json_data, container){
-		var summary = json_data.summary;
-		
-		for (var i = 0; i < presence.length ; i++){
-			var  p = presence[i];
-			var p_container = $("#"+container);
-			p_container.append('<div id="'+ p +'" style="width:' + summary[p].length + 'px;"></div>'); 
-			p_container.append("<h3>"+p+"</h3>");
-            // sort alphabetically by gene name
-            summary[p].sort(function(gene1, gene2){
-                return gene1.name.localeCompare(gene2.name);
-            });
-            $.each(summary[p], function(index, gene){
-				var occurence = parseInt(gene.value);
-				p_container.append('<span id="'+ gene.name +'" style="display: inline-block; width:50px;" title="'+ gene.name +'"><div style="background-color:' + gene.color + ';">&nbsp;&nbsp;&nbsp;&nbsp;</div><div class="text-center">' + occurence + '</div></span>');
-				p_container.append('&nbsp;');
-            });
-		}
-	};
+	    draw = function (json_data, container) {
+            var summary = json_data.summary,
+                i,
+                p,
+                p_container = $("#" + container),
+                gene_sort_alpha = function (gene1, gene2) {
+                    return gene1.name.localeCompare(gene2.name);
+                },
+                draw_gene = function (index, gene) {
+                    var occurence = parseInt(gene.value, 10),
+                        // use gene color if the number of occurences is >0
+                        color = occurence > 0 ? gene.color : "#dcdcdc";
+                    p_container.append('<span id="' + gene.name + '" style="display: inline-block; width:50px;" title="' + gene.name + '"><div style="background-color:' + color + ';">&nbsp;&nbsp;&nbsp;&nbsp;</div><div class="text-center">' + occurence + '</div></span>');
+                    p_container.append('&nbsp;');
+                };
+
+            for (i = 0; i < presence.length; i++) {
+                p = presence[i];
+                p_container.append('<div id="' + p + '" style="width:' + summary[p].length + 'px;"></div>');
+                p_container.append("<h3>" + p + "</h3>");
+                // sort alphabetically by gene name
+                summary[p].sort(gene_sort_alpha);
+                $.each(summary[p], draw_gene);
+            }
+        };
 	
 	return {
 		draw: draw
